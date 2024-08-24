7f06a8a03252707f80c0832ce615c688136af8d7
Mahbubur Rahman <mahbub.cse.me@gmail.com>
2024-08-24 12:43:54 +0600

    update

--- app/components/FacebookChatPlugin.js
+++ app/components/FacebookChatPlugin.js
@@ -10,23 +10,23 @@ const FacebookChatPlugin = ({
             MessengerRef.current.setAttribute('page_id', pageId);
             MessengerRef.current.setAttribute('attribution', 'biz_inbox');
 
-            window.fbAsyncInit = function () {
-                FB.init({
-                    xfbml: true,
-                    version: 'v17.0',
-                });
-            };
+             window.fbAsyncInit = function () {
+                 FB.init({
+                     xfbml: true,
+                     version: 'API-VERSION',
+                 });
+             };
 
-            (function (d, s, id) {
-                var js,
-                    fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s);
-                js.id = id;
-                js.src =
-                    'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-            })(document, 'script', 'facebook-jssdk');
+             (function (d, s, id) {
+                 var js,
+                     fjs = d.getElementsByTagName(s)[0];
+                 if (d.getElementById(id)) return;
+                 js = d.createElement(s);
+                 js.id = id;
+                 js.src =
+                     'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                 fjs.parentNode.insertBefore(js, fjs);
+             })(document, 'script', 'facebook-jssdk');
         }
     }, [pageId]);
 
